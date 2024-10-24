8b441e2a9cd1c02321d0f74cb6cb91b6923f7578
SinaAfzali <128758290+SinaAfzali@users.noreply.github.com>
2024-10-24 18:37:05 +0330

    create successfullCase part

---
+++ fartak/src/svgIcons/angle-left.js
@@ -0,0 +1,12 @@
+
+
+function AngleLeft(props) {
+    return (
+        <svg style={props}  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <path style={{strokeLinecap:"round",stroke:"currentColor", strokeLinejoin: "round", strokeWidth:"2"}}  d="m15 19-7-7 7-7" />
+        </svg>
+
+    )
+}
+
+export default AngleLeft
---
+++ fartak/src/svgIcons/angle-right.js
@@ -0,0 +1,11 @@
+
+
+function AngleRight(props) {
+    return (
+        <svg style={props} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <path style={{stroke: "currentcolor", strokeLinecap:"round", strokeLinejoin:"round", strokeWidth:"2"}} d="m9 5 7 7-7 7" />
+        </svg>
+    )
+}
+
+export default AngleRight
